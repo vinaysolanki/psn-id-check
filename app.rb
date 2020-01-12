e64f231d033b5bf4d5f12ead7070b2a0a06904c5
Vinay Solanki <vsolanki@MacBook-Pro.hitronhub.home>
2020-01-12 13:52:33 -0800

    Add back logger and change host

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'httparty'
 
 API_URL = "https://accounts.api.playstation.com/api/v1/accounts/onlineIds"
-HOST_URL = "psn-id-check.herokuapp.com"
+HOST_URL = "accounts.api.playstation.com"
 
 get '/' do
   erb :check
@@ -12,5 +12,6 @@ get '/check/:name' do
     resp = HTTParty.post(API_URL, 
       body: { onlineId: params['name'], reserveIfAvailable: false }.to_json,
       headers: { 'Content-Type' => 'application/json', 'Host' => HOST_URL })
+    logger.info resp
     return { body: resp.body, status: resp.code }.to_json
 end
