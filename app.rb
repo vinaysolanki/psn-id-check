c0222185dd971a834c3795514161feca110aeff3
Vinay Solanki <vsolanki@MacBook-Pro.hitronhub.home>
2020-01-12 13:38:10 -0800

    Add host header

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'httparty'
 
 API_URL = "https://accounts.api.playstation.com/api/v1/accounts/onlineIds"
+HOST_URL = "accounts.api.playstation.com"
 
 get '/' do
   erb :check
@@ -10,7 +11,7 @@ end
 get '/check/:name' do
     resp = HTTParty.post(API_URL, 
       body: { onlineId: params['name'], reserveIfAvailable: false }.to_json,
-      headers: { 'Content-Type' => 'application/json' })
+      headers: { 'Content-Type' => 'application/json', 'Host' => HOST_URL })
     logger.info resp
     return { body: resp.body, status: resp.code }.to_json
 end
