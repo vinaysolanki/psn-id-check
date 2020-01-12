fc55cfe9e9222900d8e7953c226e7f70227074b9
Vinay Solanki <vsolanki@MacBook-Pro.hitronhub.home>
2020-01-12 12:54:36 -0800

    Debug Heroku access denied issue

--- app.rb
+++ app.rb
@@ -1,14 +1,16 @@
 require 'sinatra'
 require 'httparty'
 
+API_URL = "https://accounts.api.playstation.com/api/v1/accounts/onlineIds"
+
 get '/' do
   erb :check
 end
 
 get '/check/:name' do
-  API_URL = "https://accounts.api.playstation.com/api/v1/accounts/onlineIds"
     resp = HTTParty.post(API_URL, 
       body: { onlineId: params['name'], reserveIfAvailable: false }.to_json,
       headers: { 'Content-Type' => 'application/json' })
+    Rails.logger.debug resp
     return { body: resp.body, status: resp.code }.to_json
 end
